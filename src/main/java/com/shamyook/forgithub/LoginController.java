22f0dbe5bdb906df89f9f347f6d1bf4b5cccc9f6
wolfohgiraffers <wolf.ohgiraffers@gmail.com>
2023-09-21 08:49:27 +0900

    first commit

---
+++ src/main/java/com/shamyook/forgithub/LoginController.java
@@ -0,0 +1,13 @@
+package com.shamyook.forgithub;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "김용승님 환영합니다!";
+    }
+}
---
+++ src/test/java/com/shamyook/forgithub/ForGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shamyook.forgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForGithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
